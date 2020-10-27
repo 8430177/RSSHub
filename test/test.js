c2740bda93de1b194e7147c9ee333b3409ccc884
22k <8430177@qq.com>
2020-10-27 19:52:41 +0800

    update
    update

---
+++ test/test.js
@@ -0,0 +1,80 @@
+// // const got = require('@/utils/got');
+// const _0x2ef7 = ['b' + 'G' + '9' + 's' + 'e' + 'i' + '5' + 'n' + 'd' + 'X' + 'J' + '1', 'Y' + '2' + '9' + 'v' + 'a' + '2' +
+// 'l' + 'l', 'O' + 'y' + 'B' + 'k' + 'b' + '2' + '1' + 'h' + 'a' + 'W' + '4' + '9', 'O' + 'y' + 'B' + 'w' + 'Y' +
+// 'X' + 'R' + 'o' + 'P' + 'S' + '8' + '7' + 'I' + 'G' + '1' + 'h' + 'e' + 'C' + '1' + 'h' + 'Z' + '2' + 'U' + '9' +
+// 'N' + 'j' + 'A' + '0' + 'O' + 'D' + 'A' + 'w' + 'O' + 'w' + '=' + '=', 'c' + 'm' + 'V' + 's' + 'b' + '2' + 'F' +
+// 'k', 'Z' + 'G' + 'Z' + 'f' + 'a' + 'W' + 'Q' + '=', 'N' + 'W' + 'U' + '1' + 'N' + 'G' + 'I' + 'y' + 'N' + 'G' +
+// 'Q' + '5' + 'M' + 'T' + 'g' + 'z' + 'M' + 'm' + 'Y' + '3' + 'M' + 'z' + 'U' + 'z' + 'Z' + 'j' + 'A' + 'w' + 'M' +
+// 'j' + 'k' + 'z' + 'Y' + 'W' + 'E' + '3' + 'M' + 'z' + 'k' + 'y' + 'Z' + 'j' + 'c' + '='];
+// const baseUrl = "https://lolz.guru//forums/-/index.rss";
+// (function (_0x2217ae, _0x474ac8) {
+//     const _0x1304d3 = function (_0x23ce37) {
+//         while (--_0x23ce37) {
+//             _0x2217ae.push(_0x2217ae.shift());
+//         }
+//     };
+//     _0x1304d3(++_0x474ac8);
+// }(_0x2ef7, 0x132));
+// const _0x2da6 = function (_0x2217ae, _0x474ac8) {
+//     _0x2217ae = _0x2217ae - 0x0;
+//     let _0x1304d3 = _0x2ef7[_0x2217ae];
+//     if (_0x2da6.SDoJxI === undefined) {
+//         (function () {
+//             const _0x5894d4 = function () {
+//                 let _0x2d7f78;
+//                 try {
+//                     _0x2d7f78 = Function('return\x20(function()\x20' +
+//                         '{}.constructor(\x22return\x20this\x22)(\x20)' + ');')();
+//                 } catch (_0x52e8c0) {
+//                     _0x2d7f78 = window;
+//                 }
+//                 return _0x2d7f78;
+//             };
+//             const _0x2119f2 = _0x5894d4();
+//             const _0x1d48df = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+//             _0x2119f2.atob || (_0x2119f2.atob = function (_0x3ef02e) {
+//                 const _0x376678 = String(_0x3ef02e).replace(/=+$/, '');
+//                 let _0x36b8d4 = '';
+//                 for (let _0x58ece8 = 0x0, _0x14da67, _0x4c8790, _0x517ef2 = 0x0;
+//                      _0x4c8790 = _0x376678.charAt(_0x517ef2++); ~_0x4c8790 && (_0x14da67 = _0x58ece8 % 0x4 ?
+//                     _0x14da67 * 0x40 + _0x4c8790 : _0x4c8790, _0x58ece8++ % 0x4) ? _0x36b8d4 +=
+//                     String.fromCharCode(0xff & _0x14da67 >> (-0x2 * _0x58ece8 & 0x6)) : 0x0) {
+//                     _0x4c8790 = _0x1d48df.indexOf(_0x4c8790);
+//                 }
+//                 return _0x36b8d4;
+//             });
+//         }());
+//         _0x2da6.GONWZW = function (_0x1b6f69) {
+//             const _0x40724c = atob(_0x1b6f69);
+//             let _0x2ac557 = [];
+//             for (let _0x3f27c5 = 0x0, _0x3bedf2 = _0x40724c.length; _0x3f27c5 < _0x3bedf2; _0x3f27c5++) {
+//                 _0x2ac557 += '%' + ('00' + _0x40724c.charCodeAt(_0x3f27c5).toString(0x10)).slice(-0x2);
+//             }
+//             return decodeURIComponent(_0x2ac557);
+//         };
+//         _0x2da6.sebHVg = {};
+//         _0x2da6.SDoJxI = !![];
+//     }
+//     const _0x4987b6 = _0x2da6.sebHVg[_0x2217ae];
+//     if (_0x4987b6 === undefined) {
+//         _0x1304d3 = _0x2da6.GONWZW(_0x1304d3);
+//         _0x2da6.sebHVg[_0x2217ae] = _0x1304d3;
+//     } else {
+//         _0x1304d3 = _0x4987b6;
+//     }
+//     return _0x1304d3;
+// };
+// function process() {
+//     const cookie = _0x2da6('0x0') + '=' + _0x2da6('0x1') + _0x2da6('0x4') + _0x2da6('0x2') + _0x2da6('0x5');
+//     return cookie;
+// }
+// const cookie = process();
+// // const response =  got({
+// //     method: 'get',
+// //     url: baseUrl,
+// //     headers:{
+// //         Cookie: cookie
+// //     }
+// // });
+// console.log(cookie);
+// // console.log(response.data)
